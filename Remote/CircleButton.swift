8c10dc213047564543d98c38ef646978d841f4b6
Michael Villar <me@michaelvillar.com>
2015-10-05 23:13:49 -0700

    Highlight circle

--- Remote/CircleButton.swift
+++ Remote/CircleButton.swift
@@ -21,6 +21,7 @@ class CircleButton: UIControl {
     self.key = key
     self.backgroundColor = self.mainColor
     self.layer.cornerRadius = frame.size.width / 2
+    self.clipsToBounds = false
     
     imageView = UIImageView(image: image)
     imageView.frame = self.bounds
@@ -41,9 +42,61 @@ class CircleButton: UIControl {
         
         mainColor.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha)
         self.backgroundColor = UIColor(hue: hue, saturation: min(1, saturation * 1.1), brightness: min(1, brightness * 1.3), alpha: alpha)
+        self.animateHighlightCircle()
       } else {
         self.backgroundColor = self.mainColor
       }
     }
   }
+  
+  private func animateHighlightCircle() {
+    let margin:CGFloat = 20
+    let circle = CircleHighlight(frame: CGRectInset(self.bounds, -margin, -margin), color: self.mainColor)
+    let scale:CGFloat = self.bounds.width / (self.bounds.width + margin)
+    circle.transform = CGAffineTransformMakeScale(scale, scale)
+    self.addSubview(circle)
+    
+    UIView.animateWithDuration(0.3,
+      delay: 0,
+      options: UIViewAnimationOptions.CurveEaseInOut,
+      animations: {
+        circle.transform = CGAffineTransformIdentity
+    }) { (bool) -> Void in
+    }
+    
+    UIView.animateWithDuration(0.3,
+      delay: 0,
+      options: UIViewAnimationOptions.CurveEaseIn,
+      animations: {
+        circle.alpha = 0.0
+      }) { (bool) -> Void in
+    }
+    
+
+  }
+}
+
+class CircleHighlight: UIView {
+  
+  var color: UIColor = UIColor.whiteColor()
+  
+  init(frame: CGRect, color: UIColor) {
+    super.init(frame: frame)
+    
+    self.backgroundColor = UIColor.clearColor()
+    self.color = color
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func drawRect(rect: CGRect) {
+    let lineWidth:CGFloat = 2.0
+    let path = UIBezierPath(ovalInRect: CGRectInset(self.bounds, lineWidth, lineWidth))
+    path.lineWidth = lineWidth
+    self.color.setStroke()
+    path.stroke()
+  }
+  
 }
